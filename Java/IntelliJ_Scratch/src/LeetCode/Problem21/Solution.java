c5890ecb9a3dcb4208f7e9ac188eb4b376ca1197
nber106 <nicholas.bai.berg@gmail.com>
2022-06-24 14:33:52 +1200

    Problem 21 Speed Improvement
    investigated speeding up problem 21

--- Java/IntelliJ_Scratch/src/LeetCode/Problem21/Solution.java
+++ Java/IntelliJ_Scratch/src/LeetCode/Problem21/Solution.java
@@ -13,7 +13,78 @@ public class Solution {
      *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
      * }
      */
+
+    /**
+     * Interestingly, even though the following code is neater, it is slower (1ms vs 0ms) on leetCode than the
+     * slightly messier code below it
+     */
+
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1==null) {return list2;}
+        if (list2==null) {return list1;}
+
+        ListNode listStart;
+        if (list1.val<=list2.val) {
+            listStart = list1;
+            list1 = list1.next;
+        } else {
+            listStart = list2;
+            list2 = list2.next;
+        }
+
+        ListNode listCurrentPlace = listStart;
+        while (list1!=null && list2!=null) {
+            if (list1.val <= list2.val) {
+                listCurrentPlace.next = list1;
+                list1 = list1.next;
+            } else if (list1.val > list2.val) {
+                listCurrentPlace.next = list2;
+                list2 = list2.next;
+            }
+            listCurrentPlace = listCurrentPlace.next;
+        }
+        if (list1!=null) {
+            listCurrentPlace.next = list1;
+        } else if (list2!=null) {
+            listCurrentPlace.next = list2;
+        }
+        return listStart;
+    }
+
+    public ListNode mergeTwoLists_0ms_fastest(ListNode list1, ListNode list2) {
+        if (list1==null) {return list2;}
+        if (list2==null) {return list1;}
+        ListNode combinedListStart;
+        ListNode currentCombinedList;
+        if (list1.val<=list2.val) {
+            combinedListStart = list1;
+            currentCombinedList = combinedListStart;
+            list1 = list1.next;
+        } else {
+            combinedListStart = list2;
+            currentCombinedList = combinedListStart;
+            list2 = list2.next;
+        }
+        while (list1!=null && list2!=null) {
+            if (list1.val <= list2.val) {
+                currentCombinedList.next = list1;
+                list1 = list1.next;
+            } else if (list1.val > list2.val) {
+                currentCombinedList.next = list2;
+                list2 = list2.next;
+            }
+            currentCombinedList = currentCombinedList.next;
+        }
+        if (list1!=null) {
+            currentCombinedList.next = list1;
+        }
+        if (list2!=null) {
+            currentCombinedList.next = list2;
+        }
+        return combinedListStart;
+    }
+
+    public ListNode mergeTwoLists_messy_and_slow(ListNode list1, ListNode list2) {
         if (list1==null && list2==null) {return list1;}
         ListNode combinedListStart = new ListNode();
         ListNode currentCombinedList = combinedListStart;
--- Java/IntelliJ_Scratch/src/LeetCode/Problem21/SolutionTest.java
+++ Java/IntelliJ_Scratch/src/LeetCode/Problem21/SolutionTest.java
@@ -10,5 +10,6 @@ class SolutionTest {
     void mergeTwoLists() {
         ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
         ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode list3 = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4)))));
     }
 }
